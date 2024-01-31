45f104ec525778efd1b1c8c8de6112ce828064cc
Alia ElKattan <aae322@nyu.edu>
2024-01-31 13:18:37 -0500

    script on clicking no, emoji spiral, prefernces

--- p5/emojiSpiral.js
+++ p5/emojiSpiral.js
@@ -44,7 +44,7 @@ const emojiSpiral = ( sketch ) => {
         }
 
         // let delta_t = sketch.deltaTime / 1000.0;
-        let delta_t = sketch.deltaTime / 2000.0;
+        let delta_t = sketch.deltaTime / 4000.0;
         // delta_t *= sketch.userSpeedF
         delta_t *= .5
         timer += delta_t;
